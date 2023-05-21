9e6426a61c647482421ae3eb04ebe9fea572fc16
silvermachine777 <sebastian_gg@outlook.com>
2023-05-21 08:14:52 -0500

    fix(modules): Updating  MatButtonModule import into the app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatButtonModule } from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
