7845582eb79cd8bf8289812f97e74dc18982f4b6
silvermachine777 <sebastian_gg@outlook.com>
2023-05-21 08:24:32 -0500

    feat(environment): Add apiUrl environment

--- src/app/services/flight.service.ts
+++ src/app/services/flight.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Flight } from '../models/flight.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlightService {
-  private apiUrl = 'https://recruiting-api.newshore.es/api/flights/0';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
