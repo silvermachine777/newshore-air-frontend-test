97a318b3c25e74678936be76a8eb5a762802d8e4
silvermachine777 <sebastian_gg@outlook.com>
2023-05-22 21:36:18 -0500

    feat(modules): Add MatSelectModule and FormsModule in app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FlightService } from './services/flight.service';
+import { CurrencyService } from './services/currency.service';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { SearchFormComponent } from './components/search-form/search-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule  } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatButtonModule } from '@angular/material/button';
 import { ShowRouteComponent } from './components/show-route/show-route.component';
 import {MatCardModule} from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
@@ -29,9 +32,11 @@ import {MatCardModule} from '@angular/material/card';
     HttpClientModule,
     MatSnackBarModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatSelectModule,
+    FormsModule 
   ],
-  providers: [FlightService],
+  providers: [FlightService, CurrencyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
